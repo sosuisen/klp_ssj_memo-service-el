631c0de4f5af78e9a4562e7cebf794b7f5c1a0dc
sosuisen <hidekazu.kubota@gmail.com>
2024-04-02 01:15:51 +0900

    fix: remove lombok

--- src/main/java/com/example/model/MemoDTO.java
+++ src/main/java/com/example/model/MemoDTO.java
@@ -1,11 +1,31 @@
 package com.example.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class MemoDTO {
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public MemoDTO(String author, String text, String date) {
+		this.author = author;
+		this.text = text;
+		this.date = date;
+	}
+	
 	private String author;
 	private String text;
 	private String date;
