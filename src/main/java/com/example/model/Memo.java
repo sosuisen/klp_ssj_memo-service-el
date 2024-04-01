12ab5241c2351a6418b6459367ed11a29e3b5f9d
sosuisen <hidekazu.kubota@gmail.com>
2024-04-02 01:24:02 +0900

    fix: remove lombok

--- src/main/java/com/example/model/Memo.java
+++ src/main/java/com/example/model/Memo.java
@@ -3,28 +3,6 @@ package com.example.model;
 import java.time.LocalDateTime;
 
 public class Memo {
-	public String getText() {
-		return text;
-	}
-	public void setText(String text) {
-		this.text = text;
-	}
-	public LocalDateTime getCreatedAt() {
-		return createdAt;
-	}
-	public void setCreatedAt(LocalDateTime createdAt) {
-		this.createdAt = createdAt;
-	}
-	
-	public Memo(String text, LocalDateTime createdAt) {
-		this.text = text;
-		this.createdAt = createdAt;
-	}
-	
-	// JSONのデシリアライズのためにデフォルトコンストラクタが必要
-	public Memo() {
-	}
-	
 	private String text;
 	private LocalDateTime createdAt;
 
@@ -47,7 +25,7 @@ public class Memo {
 		this.createdAt = createdAt;
 	}
 	
-	// JSONファイルの読み込み時に必要な引数なしのコンストラクタ
+	// JSONのデシリアライズのためにデフォルトコンストラクタが必要
 	public Memo() {
 	}
 }
