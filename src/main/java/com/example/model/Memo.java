5aa1d2bd61985942cdc2881c26f77ba1a9d7712f
sosuisen <hidekazu.kubota@gmail.com>
2024-04-02 01:12:51 +0900

    feat: no lombok

--- src/main/java/com/example/model/Memo.java
+++ src/main/java/com/example/model/Memo.java
@@ -2,14 +2,36 @@ package com.example.model;
 
 import java.time.LocalDateTime;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor // JSON変換時に必要
 public class Memo {
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	public Memo(String author, String text, LocalDateTime createdAt) {
+		this.author = author;
+		this.text = text;
+		this.createdAt = createdAt;
+	}
+	
+	// JSONのデシリアライズのためにデフォルトコンストラクタが必要
+	public Memo() {
+	}
+	
 	private String author;
 	private String text;
 	private LocalDateTime createdAt;
